2fe716cad965b7ef5fee775a7accdd1666d96355
Ryan Roat <ryanroat@gmail.com>
2020-07-24 14:15:31 -0400

    display github graph in my work

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
+GitHubCalendar('.calendar', 'ryanroat');
+
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
