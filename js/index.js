54fd0e07db282794f1f94fc7148d789c91e603ba
Ryan Roat <ryanroat@gmail.com>
2020-07-24 14:46:59 -0400

    github graph styling & responsive:true

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
-GitHubCalendar('.calendar', 'ryanroat');
+GitHubCalendar('.calendar', 'ryanroat', { responsive: true });
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
