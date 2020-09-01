1fa432ae2bbaedc551bfbd57c5ad5d3a5f35df4f
Ryan Roat <ryanroat@gmail.com>
2020-09-01 15:27:41 -0400

    github calendar not syncing

--- js/index.js
+++ js/index.js
@@ -4,14 +4,14 @@ const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 // const GitHubCalendar = github-calendar()
 
-GitHubCalendar('.calendar', 'ryanroat', { responsive: true });
+// GitHubCalendar('.calendar', 'ryanroat', { responsive: true });
 
 navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
+  document.body.classList.toggle('nav-open');
 });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    });
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  });
 });
